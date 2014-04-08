d02a3194dab011be84bb1722f848358dcd30e755
Jan-Michael Löw <jloew@innovative-design-solutions.de>
2014-04-08 13:46:30 +0200

    Font hinzugefügt, Formular ausgelagert, Cleanup Commit

--- Resources/Private/Templates/index.php
+++ Resources/Private/Templates/index.php
@@ -72,10 +72,11 @@
         </div>
     </div>
 </div>
+
 <div class="jumbotron white" id="wir">
     <div class="container">
-        <div class="col-sm-offset-2">
-            <div class="col-md-5">
+        <div class="row">
+            <div class="col-sm-5 col-sm-offset-1">
                 <div class="face-wrapper">
                     <img class="img img-responsive img-circle center-block" src="img/face1.png">
                     <h2>edmund salzmann</h2>
@@ -83,7 +84,7 @@
                     <p>Mit Erfahrung im Bereich Print- und Webdesign sowie Photographie seit der frühen Teenagerzeit und einem technischen Grundverständnis aus dem Studium des Wirtschaftsingenieurwesens bin ich ihr perfekter Ansprechpartner für ihr Projekt.</p>
                 </div>
             </div>
-            <div class="col-md-5">
+            <div class="col-sm-5">
                 <div class="face-wrapper">
                     <img class="img img-responsive img-circle center-block" src="img/face2.png">
                     <h2>jan-michael löw</h2>
@@ -99,6 +100,7 @@
         </div>
     </div>
 </div>
+
 <div class="jumbotron no-padding" id="referenzen">
     <div class="container">
         <div class="col-sm-offset-1">
@@ -153,43 +155,7 @@
     </div>
 </div>
 
-<div class="jumbotron white last" id="kontakt">
-    <div class="container">
-        <div class="col-sm-10 col-sm-offset-1">
-            <h2>wie sie uns erreichen</h2>
-
-            <p>Kontaktieren sie uns einfach über das untenstehende Formular oder per Telefon:</p>
-            <p style="text-align: center;">
-                +49 177 1484638
-            </p>
-        </div>
-        <div class="col-sm-8 col-sm-offset-2">
-            <div id="result"></div>
-                <form id="contact" method="post" target="php/mail.php">
-                <fieldset>
-                <div class="form-group" id="name">
-                    <label for="InputName">Ihr Name:</label>
-                    <input type="text" class="form-control" name="userName" id="userName" placeholder="Geben Sie Ihren Namen ein">
-                </div>
-                <div class="form-group" id="email">
-                    <label for="InputEmail">Ihre Email-Adresse:</label>
-                    <input type="text" class="form-control" name="userEmail" id="userEmail" placeholder="Geben Sie Ihre Email-Adresse ein">
-                </div>
-                <div class="form-group" id="phone">
-                    <label for="InputPhone">Ihre Telefonnummer: (optional)</label>
-                    <input type="text" class="form-control" name="userPhone" id="userPhone" placeholder="Geben Sie Ihre Telefonnummer ein">
-                </div>
-                <div class="form-group" id="message">
-                    <label for="InputMessage">Ihre Nachricht an uns:</label>
-                    <textarea class="form-control" rows="5" name="userMessage" id="userMessage"></textarea>
-                </div>
-
-                <button type="submit" class="btn submit" id="submit">Abschicken</button>
-                </fieldset>
-                </form>
-            </div>
-    </div>
-</div>
+<?php include_once("contact.php") ?>
 
 <div class="navbar navbar-inverse navbar-static-top footer">
     <div class="container">
